2daf04abd19fa2c79146b0e64a6583df375d6f28
misu <szijjartonagy.misu@gmail.com>
2023-05-15 21:53:36 +0200

    trainers can be created from the console

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_15_110505) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_15_195012) do
   create_table "profiles", force: :cascade do |t|
     t.string "name"
     t.date "date_of_birth"
@@ -22,6 +22,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_15_110505) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
+  create_table "trainers", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
