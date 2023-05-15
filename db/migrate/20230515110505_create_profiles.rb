6e420bbd8e720f3dd53eee8d63215d3ca68b88b4
misu <szijjartonagy.misu@gmail.com>
2023-05-15 17:25:14 +0200

    profile model

---
+++ db/migrate/20230515110505_create_profiles.rb
@@ -0,0 +1,15 @@
+class CreateProfiles < ActiveRecord::Migration[7.0]
+  def change
+    create_table :profiles do |t|
+      t.date :date_of_birth
+      t.integer :gender, limit: 1
+      t.integer :height_in_cm
+      t.integer :weight_in_kg
+      t.string :phone
+
+      t.references :user
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_15_105453) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_15_110505) do
+  create_table "profiles", force: :cascade do |t|
+    t.date "date_of_birth"
+    t.integer "gender", limit: 1
+    t.integer "height_in_cm"
+    t.integer "weight_in_kg"
+    t.string "phone"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
